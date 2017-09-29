207208f74542511744a7b29a018ff4a8fa5ed867
Julian <julian@seetransparent.com>
2017-09-29 11:39:04 +0200

    Parsing detail bodies and getting main info

--- scraper.js
+++ scraper.js
@@ -4,6 +4,7 @@ const Bluebird = require('bluebird');
 const _ = require('lodash');
 const request = Bluebird.promisify(require('request'));
 const cheerio = require('cheerio');
+const queue = require('async/queue');
 
 const WEBSITE = 'https://www.idealista.com';
 const BASE_PATHNAME = '/alquiler-viviendas/madrid/centro/malasana-universidad/';
@@ -26,7 +27,7 @@ function getBody(url) {
  * possible ajax requests that might populate the DOM (check photos).
  * It means that we miss some visible information in the website
  */
-function parseBody(body) {
+function parsePageBody(body) {
   if (_.isEmpty(body)) {
     return null;
   }
@@ -41,13 +42,49 @@ function parseBody(body) {
   $('.item').each((i, elementContainer) => {
     parsedData.listings.push({
       listingId: $(elementContainer).attr('data-adid'),
+      listingHref: $(elementContainer).find('.item-link').attr('href'),
     });
   });
 
   return parsedData;
 }
 
-const mine = Bluebird.coroutine(function* (bodyData) {
+function parseListingBody(body) {
+  if (_.isEmpty(body)) {
+    return null;
+  }
+  const $ = cheerio.load(body);
+  const parsedData = {
+    title: _.trim($('.main-info h1').text()),
+  };
+  $('.info-data span').each((i, element) => {
+    if (_.includes($(element).text(), '€/mes')) {
+      _.set(parsedData, 'price', $(element).find('.txt-big').text());
+    }
+    if (_.includes($(element).text(), 'm²')) {
+      _.set(parsedData, 'area', $(element).find('.txt-big').text());
+    }
+    if (_.includes($(element).text(), 'hab.')) {
+      _.set(parsedData, 'rooms', $(element).find('.txt-big').text());
+    }
+  });
+  const agency = $('.advertiser-data .professional-name').length;
+  _.set(parsedData, 'agency', !!agency);
+  return parsedData;
+}
+
+const detailer = queue((listing, callback) => {
+  const listingUrl = getUrlFromPathname(_.get(listing, 'listingHref'));
+  return getBody(listingUrl)
+    .then(parseListingBody)
+    .then((parsedData) => {
+      const data = _.assign({}, listing, parsedData, { url: listingUrl });
+      console.dir(data);
+    })
+    .finally(() => callback());
+});
+
+const mine = async function (bodyData) {
   let url;
   if (_.isEmpty(bodyData)) {
     url = BASE_PATHNAME;
@@ -55,15 +92,15 @@ const mine = Bluebird.coroutine(function* (bodyData) {
     url = _.get(bodyData, 'pagination.nextPageHref');
   }
 
-  const body = yield getBody(getUrlFromPathname(url));
-  const pageData = parseBody(body);
+  const body = await getBody(getUrlFromPathname(url));
+  const pageData = parsePageBody(body);
   if (_.get(pageData, 'listings.length')) {
-    console.log(pageData.listings);
+    detailer.push(pageData.listings);
   }
   if (_.get(pageData, 'pagination.nextPageHref')) {
-    yield mine(pageData);
+    await mine(pageData);
   }
-});
+};
 
 mine()
   .catch(err => console.error(err));
